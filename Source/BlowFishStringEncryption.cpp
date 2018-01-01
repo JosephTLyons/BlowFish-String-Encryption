da72a5d9107dd53a9a81325f72956fdfc474ffa0
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-01 18:40:21 -0500

    Initial Commit

---
+++ Source/BlowFishStringEncryption.cpp
@@ -0,0 +1,31 @@
+//
+//  BlowFishStringEncryption.cpp
+//  BlowFish String Encryption - ConsoleApp
+//
+//  Created by Joseph Lyons on 1/1/18.
+//
+
+#include "BlowFishStringEncryption.h"
+
+String encryptStringWithBlowFish (const String &key, const String &textToEncrypt)
+{
+    MemoryBlock mb;
+    MemoryOutputStream os (mb, false);
+    os << textToEncrypt;
+    
+    BlowFish blowFish (key.toUTF8(), (int) key.getNumBytesAsUTF8());
+    blowFish.encrypt (mb);
+    
+    return mb.toBase64Encoding();
+}
+
+String decryptStringWithBlowFish (const String &key, String &textToDecrypt)
+{
+    MemoryBlock mb;
+    mb.fromBase64Encoding(textToDecrypt);
+    
+    BlowFish blowFish (key.toUTF8(), (int) key.getNumBytesAsUTF8());
+    blowFish.decrypt (mb);
+    
+    return mb.toString();
+}
---
+++ Source/BlowFishStringEncryption.h
@@ -0,0 +1,17 @@
+//
+//  BlowFishStringEncryption.h
+//  BlowFish String Encryption
+//
+//  Created by Joseph Lyons on 1/1/18.
+//
+
+#ifndef BlowFishStringEncryption_h
+#define BlowFishStringEncryption_h
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+String encryptStringWithBlowFish (const String &key, const String &textToEncrypt);
+String decryptStringWithBlowFish (const String &key, String &textToDecrypt);
+
+
+#endif /* BlowFishStringEncryption_h */
